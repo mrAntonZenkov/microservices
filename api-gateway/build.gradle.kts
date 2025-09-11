bc785c49f98156434d8543d473e8469511759e54
Anton Zenkov <antonziankou86@gmail.com>
2025-09-11 13:51:11 +0300

    init user-service

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
